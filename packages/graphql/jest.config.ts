747b1cab0f663df7d793573b2784fa775a7937e4
Khanh Bui <khanh.bh@rockyhub.io>
2023-05-16 08:45:47 +0700

    rename libs => packages

---
+++ packages/graphql/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'graphql',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/packages/graphql',
+};
